3d3e7049c7750b200ce8ffe3e8314394d9f1213d
Hector Choi <hctrchoi@gmail.com>
2022-04-18 20:43:04 -0700

    advanced ex 3 with vim

---
+++ small_problems/advanced1/ex3.rb
@@ -0,0 +1,20 @@
+def transpose(arr)
+  transposed = []
+  arr.size.times do |row_size|
+    transposed_column = []
+    arr.first.size.times do |column_size|
+      transposed_column << arr[column_size][row_size] 
+    end
+    transposed << transposed_column
+  end
+  transposed
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
